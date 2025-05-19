089c56f4ceaa8ae1d2a2ce07f670eaaac79895b4
julia-anjos968 <julia.r.anjos@aluno.senai.br>
2025-05-19 10:58:56 -0300

    first commit

--- scripts/questao-01.js
+++ scripts/questao-01.js
@@ -1,1 +1,24 @@
-// depois vou fazer questão 1
+const numeros = [12, 5, 8, 21, 16, 7, 30, 45, 13, 27];
+let numerosPares = 0;
+let numerosImpares = 0;
+let maioresQue20 = 0;
+
+// Varredura do array 
+for (let i = 0; i < numeros.length; i++) {
+   //condicional para números pares
+    if (numeros[i] % 2 == 0) {
+        numerosPares++;
+    } else {
+        numerosImpares++;
+    }
+    // condicional para maiores que 20
+    if (numeros[i] > 20) {
+maioresQue20++;
+    }
+}
+
+// numerosImpares = numeros.length - numerosPares;
+
+console.log(`A quantidade de números pares é: ${numerosPares}`)
+console.log(`A quantidade de números ímpares é: ${numerosImpares}`)
+console.log(`A quantidade de números maiores que 20 é: ${maioresQue20}`)
